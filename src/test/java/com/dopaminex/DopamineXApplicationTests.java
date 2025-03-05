c1b50dbab87a67950eb1b2f456712901bdaa0824
Rabbokki <j55267035@gmail.com>
2025-03-05 17:53:32 +0900

    기본 뼈대 커밋

---
+++ src/test/java/com/dopaminex/DopamineXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopaminex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopamineXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
