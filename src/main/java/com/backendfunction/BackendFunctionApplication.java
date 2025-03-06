49303e12235104122de41259824ef3e4f83f3f7d
rabbokki <j55267035@gmail.com>
2025-03-06 18:26:44 +0900

    first 커밋

---
+++ src/main/java/com/backendfunction/BackendFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.backendfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFunctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backendfunction/BackendFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFunctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
