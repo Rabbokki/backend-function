bec65bbef38fbcfc3c9b62c55eaa104e9188e69e
seegeegaja <wnsdyd821@gmail.com>
2025-03-27 12:46:16 +0900

    cart 완료

--- src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
+++ src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
@@ -72,6 +72,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/ws/**").permitAll()
 //                        .requestMatchers("/post/create").permitAll()
                         .requestMatchers("/post/**").permitAll()
+                        .requestMatchers("/cart/**").authenticated()
                         .requestMatchers("/chat").authenticated()
                         .requestMatchers("/api/liquor/**").permitAll()
                         .anyRequest().authenticated())
