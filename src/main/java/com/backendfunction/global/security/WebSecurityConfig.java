00ad584c58b51dbe84c0fe585867f949d519cf6a
Rabbokki <j55267035@gmail.com>
2025-04-06 15:35:42 +0900

    api 요청 절대경로 수정

--- src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
+++ src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
@@ -47,7 +47,10 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.36.33.178"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
+                "http://3.36.33.178",
+                "http://3.36.33.178:80",
+                "http://3.36.33.178:8081"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*", "Access_Token"));
         configuration.setAllowCredentials(true);
