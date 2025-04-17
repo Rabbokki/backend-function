4ecfdaf20d1f4d658acb7866a8dc620b64f78eb1
Rabbokki <j55267035@gmail.com>
2025-04-17 12:07:07 +0900

    재배포2

--- src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
+++ src/main/java/com/backendfunction/global/security/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://52.79.225.242"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://43.200.97.196"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*", "Access_Token"));
         configuration.setAllowCredentials(true);
